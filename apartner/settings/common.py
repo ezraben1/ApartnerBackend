6a9c9790909dae1ce5cba9f2e98468c773d8ef22
Ben Ezra <118681190+ezraben1@users.noreply.github.com>
2023-05-11 17:42:30 +0300

    Prepare for Heroku deployment

--- apartner/settings/common.py
+++ apartner/settings/common.py
@@ -68,6 +68,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8001",
     "http://127.0.0.1:8001",
     "http://localhost:5173",
+    "http://localhost:3000",
+    "https://apartnerfrontend.herokuapp.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
