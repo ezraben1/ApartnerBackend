44ea924dc48b144565e411a59b29c008b296e387
Ben Ezra <118681190+ezraben1@users.noreply.github.com>
2023-05-11 14:24:36 +0300

    Add django-filter to dependencies

--- apartner/settings/common.py
+++ apartner/settings/common.py
@@ -143,6 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+if not os.path.exists(STATIC_ROOT):
+    os.makedirs(STATIC_ROOT)
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -150,6 +152,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
